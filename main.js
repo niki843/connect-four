f5d7dc3d04c01dc533db171538533dc385e1a84a
niki843 <niki.tornev96@gmail.com>
2022-10-20 21:00:02 +0300

    Fixed url for page

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 function getWebSocketServer() {
-  if (window.location.host === "aaugustin.github.io") {
+  if (window.location.host === "niki843.github.io") {
     return "wss://websockets-tutorial.herokuapp.com/";
   } else if (window.location.host === "localhost:8000") {
     return "ws://localhost:8001/";
